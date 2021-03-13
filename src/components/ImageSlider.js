bf675c1f6a4dfa52a8f727802c861d9993f342c7
lucastroncoso <lucastroncoso.seguros@gmail.com>
2021-03-13 13:12:06 -0300

    Ya anda

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -3,8 +3,15 @@ import { SliderData } from './SliderData';
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 const ImageSlider = ({ slides }) => {
-  const [current, setCurrent] = useState(0);
+
   const length = slides.length;
+  let windowWidth = window.innerWidth;
+
+  
+  const [current, setCurrent] = useState(0);
+  const [touchStart, setTouchStart] = React.useState(0);
+  const [touchEnd, setTouchEnd] = React.useState(0);
+
 
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
@@ -14,19 +21,53 @@ const ImageSlider = ({ slides }) => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
 
+  function handleTouchStart(e) {
+    setTouchStart(e.targetTouches[0].clientX);
+  }
+
+  function handleTouchMove(e) {
+    setTouchEnd(e.targetTouches[0].clientX);
+  }
+
+  function handleTouchEnd() {
+    if (touchStart - touchEnd > 150) {
+      // do your stuff here for left swipe
+      nextSlide();
+    }
+
+    if (touchStart - touchEnd < -150) {
+      // do your stuff here for right swipe
+      prevSlide();
+    }
+  }
+
+
+
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
 
   return (
     <section className='slider'>
-      <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
-      <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+
+      {
+        windowWidth > 740 ?
+          <>
+            <FaArrowAltCircleLeft className='left-arrow' onClick={prevSlide} />
+            <FaArrowAltCircleRight className='right-arrow' onClick={nextSlide} />
+          </>
+          :
+          null
+      }
+
       {SliderData.map((slide, index) => {
         return (
           <div
             className={index === current ? 'slide active' : 'slide'}
             key={index}
+            onTouchStart={touchStartEvent => handleTouchStart(touchStartEvent)}
+            onTouchMove={touchMoveEvent => handleTouchMove(touchMoveEvent)}
+            onTouchEnd={() => handleTouchEnd()}
           >
             {index === current && (
               <img src={slide.image} alt='travel image' className='image' />
